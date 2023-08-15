f09967d03975b5a0ff631c9d1795f720e8fc28bb
0sugo <josugo26@gmail.com>
2023-08-16 00:56:19 +0300

    Fix rubocop offences

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MessagesController < ApplicationController
   def random_greeting
-    @random = Message.order("RANDOM()").first
+    @random = Message.order('RANDOM()').first
 
     if @random
-      render json: {status:'Success', message:"Message fetched succesfully",data: @random.greeting },status: :ok
+      render json: { status: 'Success', message: 'Message fetched succesfully', data: @random.greeting }, status: :ok
     else
-      render json: {status:'Failed', message: "Failed to fetch Messages",data: @random&.errors },status: :bad_request
+      render json: { status: 'Failed', message: 'Failed to fetch Messages', data: @random&.errors }, status: :bad_request
     end
   end
 end
